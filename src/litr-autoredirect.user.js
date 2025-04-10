aaea256f1da2e516697599557f739a8d918b3539
duke <duke.nsk@gmail.com>
2025-04-10 15:26:53 +0700

    fix: match URL

--- src/litr-autoredirect.user.js
+++ src/litr-autoredirect.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Listen Tracker Autoredirect
 // @namespace    litr-autoredirect
-// @version      1.00
+// @version      1.01
 // @description  Automatic instant redirect, instead of 10-second waiting on the Litr.CC website
 // @author       Duke
-// @match        https://litr.cc/t/nr/*
+// @match        https://litr.cc/t/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=litr.cc
 // @grant        none
 // ==/UserScript==
